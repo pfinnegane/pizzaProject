78d592a766911058d6d82250c17b0937596d55c9
Patrick Finnegan <pfinnegan.dev@outlook.com>
2019-09-17 20:57:45 -0400

    Made menu prettier

--- pageFormat.php
+++ pageFormat.php
@@ -4,30 +4,36 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title></title>
   </head>
   <body>
   	<?php
   	
-    function pageHeader($title = "Dominik's",$img = "logo.png")
-    {
-       echo<<<EOT
-        <div class="row">
-          <img src="../images/$img"  style="height: 80px;>
-          <h2 class="mt-2">$title</h2>
-          <nav class="navbar ">
-          <!-- Navbar content -->
-              <a class="nav-item nav-link active" href="../index.php">Home <span class="sr-only">(current)</span></a>
-              <a class="nav-item nav-link" href="./pages/menu.php">Menu</a>
-              <a class="nav-item nav-link" href="#">Orders</a>
-               <a class="nav-item nav-link" href="#">Login</a>
-                <a class="nav-item nav-link" href="#">Signup</a>
-              <a class="nav-item nav-link disabled" href="#">More</a>
-            </nav>
-        </div>
-        <hr class="style1"/>
+function pageHeader($title = "Dominik's",$img = "logo.png"){
+echo<<<EOT
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+<img class="navbar-brand" src="../images/$img"  style="height: 80px;">
+<h2>$title</h2>
+<!-- Navbar content -->
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+    <div class="collapse navbar-collapse id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active"><a class="nav-link" href="../">Home <span class="sr-only">(current)</span></a></li>
+      <li class="nav-item"> <a class="nav-link" href="./pages/menu.php">Menu</a></li>
+      <li class="nav-item"> <a class="nav-link" href="#">Orders</a></li>
+      <li class="nav-item"> <a class="nav-link" href="#">Login</a></li>
+      <li class="nav-item"> <a class="nav-link" href="#">Signup</a></li>
+      <li class="nav-item"> <a class="nav-link disabled" href="#">More</a></li>
+    </ul>
+    </div>
+  </nav>
 EOT;
-     }
+}
 
      function pageFooter($img)
      {
--- pages/menu.php
+++ pages/menu.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/main.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Menu</title>
 </head>
@@ -19,14 +22,18 @@
         
     function printCard($img, $price, $pn, $description){   
         echo<<<_EOT
-            <div class="col-sm-4">
+            <div class="col-xs-7 col-md-4 mt-3 card-group">
             <div class="card">
-                <img class="card-img-top" style="max-height:300px; width: auto;" src="$img"> <p>$price</p>
-                <h5 class="card-body">
-                    <h5 class="card-title">$pn</h5>
+                <img class="card-img-top" style="max-height: 230px;object-fit: cover; overflow-y:hidden;" src="$img"> 
+                <div class="card-body">
+                    <h5 class="card-title" style="text-transform:capitalize;">$pn</h5>
                     <p class="card-text">$description</p>
+                </div>
+                <div class="card-footer"><small class="text-muted align-middle">$$price</small><span class="float-right align-middle btn btn-primary">Add to cart</span></div>
             </div>
+            
             </div>
+            
 _EOT;
     } 
     $cols = 3;
@@ -39,15 +46,13 @@ _EOT;
         $price= $item['price'];
         $toppings = $item['topping'];
         $description = "A pizza with ".$toppings;
-        if($col==0) echo "<div class=\"row \">";
+        if($col==0) echo "<div class=\"row\">";
         
         printCard($img, $price, $pn, $description); 
         $col++;
         if($col >= $cols) {
             echo "</div>";
             $col=0;
-            echo "<div class=\"space10\"></div>";
-            echo "<hr>";
         }
         
 }
